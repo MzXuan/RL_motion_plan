f5c59c94e246509d5da529447f60a2fe409709c7
xuanzxzhao <xuanzxzhao@tencent.com>
2020-09-11 22:09:20 +0800

    add path gen for test

--- gym_rlmp/envs/ws_path_gen.py
+++ gym_rlmp/envs/ws_path_gen.py
@@ -9,37 +9,40 @@ class WsPathGen():
     def __init__(self, start, end):
         self.start = start
         self.end = end
-        self.path = self.sample_path(start, end)
+        self.sample_path(start, end)
 
 
     def sample_path(self, start, end):
         waypoints = np.linspace(start, end, num=10)
         waypoints += (np.random.rand(waypoints.shape[0],waypoints.shape[1])-0.5)*0.05
-
-        return waypoints
+        self.path = waypoints
 
 
     def next_goal(self, center, r):
         dists = np.linalg.norm((self.path-center),axis=1)
         print("dist is: ", dists)
         indices = np.where(dists < r)[0]
+
         print("indices", indices)
 
+        inverse_indices = np.flip(indices)
 
-        for i in indices:
-            if i+1 < len(dists):
+        for i in inverse_indices: # from end to start
+            if i+1 < len(dists): # not meet limit
                 print("current i is: ", i)
-                self.calculate_interaction(center, r, self.path[i], self.path[i+1])
-
+                p_insect = self.calculate_interaction(center, r, self.path[i], self.path[i+1])
+                if p_insect is not None:
+                    print("returned intersection is: ", p_insect)
+                    return p_insect
             else:
                 #the end, return the last way point
                 return self.path[-1]
 
         #no interection, return the waypoint with minimum distance
         # todo: or expand r please
+        ####!!!!todo!!!! problem here######
         idx = np.argmin(dists)
         return self.path[idx]
-
 
 
     def calculate_interaction(self, center, r, p0, p1):
@@ -69,19 +72,12 @@ class WsPathGen():
             print("solved root are: ", t_roots)
             print("t_cons are: ", t_cons)
             #find intersection
-            point_intersect = p0*[1-t]+p1
+            point_intersect = p0*[1-t]+p1*t
+            print("p0 {} and p1 {}".format(p0,p1))
             print("point_intersect is: ", point_intersect)
+            return point_intersect
 
 
-
-
-
-
-
-#create cube vertex
-
-def create_cube(center, d):
-    return 0
 
 def create_ball(center, r):
     # Create a sphere
@@ -95,33 +91,19 @@ def create_ball(center, r):
     return x,y,z
 
 
-
-if __name__ == '__main__':
-    start = [0.5,0.6,0.1]
-    end =[0.1,0.5,0.3]
-    ws_path_gen = WsPathGen(start, end)
-    waypoints = ws_path_gen.sample_path(start, end)
-
-    center = waypoints[2,:]
-    radius = 0.05
-    ws_path_gen.next_goal(center, radius)
-
-
-    print("waypoints", waypoints)
-    print("shape of way points", waypoints.shape)
-
-    ws_path_gen.next_goal(center, radius)
-
-
+def plot_result(waypoints, center,  goal):
     #---- plot ---#
     fig = plt.figure()
     ax = plt.axes(projection="3d")
 
-    bx, by, bz = create_ball(waypoints[2,:], 0.05)
+    bx, by, bz = create_ball(center, 0.05)
     ax.plot_surface(
         bx, by, bz, rstride=1, cstride=1, color='c', alpha=0.6, linewidth=0)
 
     ax.plot(waypoints[:,0], waypoints[:,1], waypoints[:,2],'-*')
+
+    #print intersection point
+    ax.plot([goal[0]], [goal[1]], [goal[2]],'+')
     ax.set_xlabel("x")
     ax.set_ylabel("y")
     ax.set_zlabel("z")
@@ -129,5 +111,25 @@ if __name__ == '__main__':
     ax.set_ylim(0, 0.8)
     ax.set_zlim(0, 0.8)
     plt.show()
+
+
+
+
+if __name__ == '__main__':
+    start = [0.5,0.6,0.1]
+    end =[0.1,0.5,0.3]
+    ws_path_gen = WsPathGen(start, end)
+    waypoints = ws_path_gen.path
+
+    center = waypoints[0,:]
+    radius = 0.05
+
+    for _ in range(0,20):
+        ws_path_gen.next_goal(center, radius)
+        goal = ws_path_gen.next_goal(center, radius)
+        plot_result(waypoints, center, goal)
+        center = goal+np.random.uniform(-0.05, 0.05)
+
+
 
 
