c61b8aea1fd0ab4ff4c2678cf98fd986983c0642
xuanzxzhao <xuanzxzhao@tencent.com>
2020-10-17 22:40:24 +0800

    add kalman filter

--- pybullet_gym/pybullet_ur5/envs/human_connection.py
+++ pybullet_gym/pybullet_ur5/envs/human_connection.py
@@ -2,6 +2,53 @@ from kinect2.client import Kinect2Client
 import time
 import json
 import numpy as np
+from filterpy.kalman import KalmanFilter
+
+
+def kf_filter():
+    dt = 1 / 30
+    kf = KalmanFilter(dim_x=6, dim_z=6, dim_u=3)
+    kf.F = np.array([[1, 0, 0, dt, 0, 0],
+                     [0, 1, 0, 0, dt, 0],
+                     [0, 0, 1, 0, 0, dt],
+                     [0, 0, 0, 1, 0, 0],
+                     [0, 0, 0, 0, 1, 0],
+                     [0, 0, 0, 0, 0, 1]])
+
+    kf.B = np.array([[0.5 * dt ** 2, 0, 0],
+                     [0, 0.5 * dt ** 2, 0],
+                     [0, 0, 0.5 * dt ** 2],
+                     [dt, 0, 0],
+                     [0, dt, 0],
+                     [0, 0, dt]])
+
+    kf.H = np.eye(6)
+
+    kf.P = np.array([[1.00199823e+00, 0.00000000e+00, 0.00000000e+00, 6.65341280e-05,
+                      0.00000000e+00, 0.00000000e+00],
+                     [0.00000000e+00, 1.00199823e+00, 0.00000000e+00, 0.00000000e+00,
+                      6.65341280e-05, 0.00000000e+00],
+                     [0.00000000e+00, 0.00000000e+00, 1.00199823e+00, 0.00000000e+00,
+                      0.00000000e+00, 6.65341280e-05],
+                     [6.65341280e-05, 0.00000000e+00, 0.00000000e+00, 1.00199602e+00,
+                      0.00000000e+00, 0.00000000e+00],
+                     [0.00000000e+00, 6.65341280e-05, 0.00000000e+00, 0.00000000e+00,
+                      1.00199602e+00, 0.00000000e+00],
+                     [0.00000000e+00, 0.00000000e+00, 6.65341280e-05, 0.00000000e+00,
+                      0.00000000e+00, 1.00199602e+00]])  # covariance matrix
+    kf.Q = np.eye(6) * 0.2  # process matrix (小=相信模型）
+
+    kf.R = np.array([[0.2, 0, 0, 0, 0, 0],
+                     [0, 0.2, 0, 0, 0, 0],
+                     [0, 0, 0.2, 0, 0, 0],
+                     [0, 0, 0, 3, 0, 0],
+                     [0, 0, 0, 0, 3, 0],
+                     [0, 0, 0, 0, 0, 3]])  # measurement uncertantity
+
+    return kf
+
+
+
 
 
 class HumanModel(object):
@@ -12,9 +59,30 @@ class HumanModel(object):
         self.kinect.skeleton.start()
         self.joints = {}
         self.joint_velocity = {}
-        self.dt = 1/30
+        self.joint_accerlation = {}
+
+        self.joint_queue = {}
+
+        # self.dt = 1/30
         self.human_id = None
-        self.time_stamp = time.time()
+        self.last_time_stamp = time.time()
+        self.filters = {'ElbowLeft': kf_filter(), 'HandLeft': kf_filter()} #elbow, hand
+        self.filter_joint_name = ['ElbowLeft', 'HandLeft']
+
+
+    def reset(self):
+        self.joints = {}
+        self.joint_velocity = {}
+        self.joint_accerlation = {}
+
+        self.joint_queue = {}
+
+        # self.dt = 1/30
+        self.human_id = None
+        self.last_time_stamp = time.time()
+        self.filters = {'ElbowLeft': kf_filter(), 'HandLeft': kf_filter()}  # elbow, hand
+        self.filter_joint_name = ['ElbowLeft', 'HandLeft']
+
 
 
     def get_joint_state(self, joint_name):
@@ -24,30 +92,79 @@ class HumanModel(object):
     def callback_skeleton(self, msg):
         human_id = list(msg.keys())[0]
 
-        self.time_stamp = time.time()
+
+        current_time =time.time()
+        dt = current_time-self.last_time_stamp
+
+        if dt>1:
+            self.reset()
 
         # print("human id: ", human_id)
         value_dict = msg[human_id]
 
-        # # check is this the previous human
-        # if self.human_id is None:
-        #     self.human_id = human_id
-        # else:
-        #     if self.human_id != human_id:
-        #         print("human id not match")
-        #         return
+        # check is this the previous human
+        if self.human_id is None:
+            self.human_id = human_id
+        else:
+            if self.human_id != human_id:
+                print("human id not match")
+                return
 
         #copy data to joint info
         for joint_name, value in value_dict.items():
 
             try:
                 last_joint_position = self.joints[joint_name].copy()
-
-                self.joints[joint_name] = np.asarray([value['Position']['X'], value['Position']['Y'], value['Position']['Z']])
-                self.joint_velocity[joint_name] = (self.joints[joint_name]-last_joint_position)/self.dt
+                self.joints[joint_name] = np.asarray(
+                    [value['Position']['X'], value['Position']['Y'], value['Position']['Z']])
             except:
-                self.joints.update({joint_name:np.asarray([value['Position']['X'], value['Position']['Y'], value['Position']['Z']])})
-                self.joint_velocity.update({joint_name:0})
+                last_joint_position = self.joints[joint_name] = np.asarray(
+                    [value['Position']['X'], value['Position']['Y'], value['Position']['Z']])
+
+            self.joint_velocity[joint_name] = (self.joints[joint_name] - last_joint_position) / dt
+            self.joint_accerlation[joint_name] = self.joint_velocity[joint_name] / dt
+
+            # --- add kalman filter----#
+            if joint_name in self.filter_joint_name:
+
+                vel = self.joint_velocity[joint_name]
+                acc = self.joint_accerlation[joint_name]
+
+                if np.linalg.norm(vel) > 3:
+                    vel = 3 * vel / np.linalg.norm(vel)
+                if np.linalg.norm(acc) > 10:
+                    acc = 10 * acc / np.linalg.norm(acc)
+
+                #prepare kalman filter
+                self.filters[joint_name].F = np.array([[1, 0, 0, dt, 0, 0],
+                                 [0, 1, 0, 0, dt, 0],
+                                 [0, 0, 1, 0, 0, dt],
+                                 [0, 0, 0, 1, 0, 0],
+                                 [0, 0, 0, 0, 1, 0],
+                                 [0, 0, 0, 0, 0, 1]])
+
+                self.filters[joint_name].B = np.array([[0.5 * dt ** 2, 0, 0],
+                                 [0, 0.5 * dt ** 2, 0],
+                                 [0, 0, 0.5 * dt ** 2],
+                                 [dt, 0, 0],
+                                 [0, dt, 0],
+                                 [0, 0, dt]])
+
+                z= np.concatenate([self.joints[joint_name], vel])
+                history_joint = self.filters[joint_name].x.copy()[:3]
+                #update kalman filter
+                self.filters[joint_name].predict(acc)
+                self.filters[joint_name].update(z)
+                current_joint = self.filters[joint_name].x.copy()[:3]
+
+                #predict step
+                next_joint = self.filters[joint_name].get_prediction(acc)[0][:3]
+
+                self.joint_queue[joint_name] = [history_joint, current_joint, next_joint]
+
+            self.last_time_stamp = current_time
+
+
 
         # print("value dict", self.joints)
         # print("joint velocity", self.joint_velocity['HandRight'])
@@ -58,16 +175,16 @@ if __name__ == "__main__":
 
     joint_name = ['ElbowLeft', 'HandLeft']
     csv_data = []
-    last_time = hm.time_stamp
+    last_time = hm.last_time_stamp
     while True:
         try:
             time.sleep(0.03)
-            if hm.time_stamp!=last_time:
+            if hm.last_time_stamp!=last_time:
                 csv_data.append(np.concatenate([
-                    np.asarray([hm.time_stamp]), hm.joints['ElbowLeft'], hm.joints['HandLeft']]))
-
+                    np.asarray([hm.last_time_stamp]), hm.joints['ElbowLeft'], hm.joints['HandLeft']]))
+            last_time = hm.last_time_stamp
 
         except KeyboardInterrupt:
-            np.savetxt("/home/xuan/Documents/human_data.csv", np.asarray(csv_data), delimiter=",")
-            print("save csv successfully")
+            # np.savetxt("/home/xuan/Documents/human_data.csv", np.asarray(csv_data), delimiter=",")
+            # print("save csv successfully")
             raise
--- pybullet_gym/pybullet_ur5/envs/skeleton_filter.py
+++ pybullet_gym/pybullet_ur5/envs/skeleton_filter.py
@@ -1,7 +1,10 @@
 from filterpy.kalman import KalmanFilter
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.signal import butter, filtfilt
 
+import time
 
 
 def load_csv():
@@ -13,13 +16,14 @@ def load_csv():
 #todo: dt need to be updated from origin time steps
 def kf_filter():
     dt = 1/30
-    kf = KalmanFilter(dim_x=6, dim_z=3, dim_u=3)
+    kf = KalmanFilter(dim_x=6, dim_z=6, dim_u=3)
     kf.F = np.array([[1, 0, 0, dt, 0, 0],
                      [0, 1, 0, 0, dt, 0],
                      [0, 0, 1, 0, 0, dt],
                      [0, 0, 0, 1, 0,  0],
                      [0, 0, 0, 0, 1,  0],
                      [0, 0, 0, 0, 0,  1]])
+
 
     kf.B = np.array([[0.5*dt**2, 0, 0],
                      [0, 0.5*dt**2, 0],
@@ -28,19 +32,155 @@ def kf_filter():
                      [0, dt, 0],
                      [0, 0, dt]])
 
-    kf.H = np.array([[1, 0, 0, 0, 0, 0],
-                    [0, 1, 0, 0, 0, 0],
-                    [0, 0, 1, 0, 0, 0]])
+
+    # kf.H = np.array([[1, 0, 0, 0, 0, 0],
+    #                 [0, 1, 0, 0, 0, 0],
+    #                 [0, 0, 1, 0, 0, 0]])
+
+    kf.H = np.eye(6)
+
+    # kf.P = np.eye(6)*0.01
+
+    kf.P = np.array([[1.00199823e+00, 0.00000000e+00, 0.00000000e+00, 6.65341280e-05,
+                           0.00000000e+00, 0.00000000e+00],
+                          [0.00000000e+00, 1.00199823e+00, 0.00000000e+00, 0.00000000e+00,
+                           6.65341280e-05, 0.00000000e+00],
+                          [0.00000000e+00, 0.00000000e+00, 1.00199823e+00, 0.00000000e+00,
+                           0.00000000e+00, 6.65341280e-05],
+                          [6.65341280e-05, 0.00000000e+00, 0.00000000e+00, 1.00199602e+00,
+                           0.00000000e+00, 0.00000000e+00],
+                          [0.00000000e+00, 6.65341280e-05, 0.00000000e+00, 0.00000000e+00,
+                           1.00199602e+00, 0.00000000e+00],
+                          [0.00000000e+00, 0.00000000e+00, 6.65341280e-05, 0.00000000e+00,
+                           0.00000000e+00, 1.00199602e+00]]) #covariance matrix
+    kf.Q = np.eye(6)*0.5#process matrix (小=相信模型）
+
+
+
+    kf.R = np.array([[0.2, 0, 0, 0, 0, 0],
+                         [0, 0.2, 0, 0, 0, 0],
+                         [0, 0, 0.2, 0, 0, 0],
+                         [0, 0, 0, 3, 0, 0],
+                         [0, 0, 0, 0, 3, 0],
+                         [0, 0, 0, 0, 0, 3]]) #measurement uncertantity
+
+
     return kf
 
-
-
-#todo: initial value for filter
+#
+# def butter_lowpass_filter(data, cutoff, fs, order):
+#     nyq = 0.5 * fs  # Nyquist Frequency
+#     normal_cutoff = cutoff / nyq
+#     # Get the filter coefficients
+#     b, a = butter(order, normal_cutoff, btype='low', analog=False)
+#     y = filtfilt(b, a, data, padlen=0)
+#     return y
 
 data_frame = load_csv()
 timesteps = (data_frame.loc[:,0]).to_numpy()
-elbow = (data_frame.loc[:, 1:4]).to_numpy()
-hand = (data_frame.loc[:, 4:7]).to_numpy()
+# timesteps = timesteps[:]-timesteps[0]
+
+elbow = (data_frame.loc[:, 1:3]).to_numpy()
+hand = (data_frame.loc[:, 4:6]).to_numpy()
+
+kf_elbow = kf_filter()
+kf_hand = kf_filter()
+
+pos_hand = hand[0, :]
+vel_hand=acc_hand = np.zeros(3)
+
+kf_hand.x = np.concatenate([pos_hand, vel_hand]).T
+
+
+measure_x = []
+filter_x = []
+pred_x = []
+acc_list = []
+vel_list = []
+
+
+
+for i in range(1, len(timesteps)):
+    #simulate online process
+    pos_elbow = elbow[i, :]
+    pos_hand = hand[i, :]
+
+    if timesteps[i] == timesteps[i-1]:
+        vel_elbow = acc_elbow = vel_hand = acc_hand = np.zeros(3)
+        dt = 1/30
+    else:
+        # vel_elbow = elbow[i,:]/(timesteps[i]-timesteps[i-1])
+        # acc_elbow = vel_elbow/(timesteps[i]-timesteps[i-1])
+        vel_hand = (hand[i, :]-hand[i-1,:]) / (timesteps[i] - timesteps[i - 1])
+        acc_hand = vel_hand / (timesteps[i] - timesteps[i - 1])
+        dt = (timesteps[i] - timesteps[i - 1])
+
+    if np.linalg.norm(vel_hand)>3:
+        vel_hand = 3*vel_hand/np.linalg.norm(vel_hand)
+    if np.linalg.norm(acc_hand)>10:
+        acc_hand = 10*acc_hand/np.linalg.norm(acc_hand)
+
+
+    acc_list.append(acc_hand)
+    vel_list.append(vel_hand)
+
+    # z =np.expand_dims(np.concatenate([pos_hand, vel_hand]), axis=1)
+    z = np.concatenate([pos_hand, vel_hand]).T
+
+    s_time = time.time()
+
+    # #prepare
+    kf_hand.F = np.array([[1, 0, 0, dt, 0, 0],
+                                 [0, 1, 0, 0, dt, 0],
+                                 [0, 0, 1, 0, 0, dt],
+                                 [0, 0, 0, 1, 0, 0],
+                                 [0, 0, 0, 0, 1, 0],
+                                 [0, 0, 0, 0, 0, 1]])
+
+    kf_hand.B = np.array([[0.5 * dt ** 2, 0, 0],
+                                 [0, 0.5 * dt ** 2, 0],
+                                 [0, 0, 0.5 * dt ** 2],
+                                 [dt, 0, 0],
+                                 [0, dt, 0],
+                                 [0, 0, dt]])
+
+    # update
+    kf_hand.predict(u=acc_hand)
+    kf_hand.update(z=z)
+
+    # print("x current is: ", z)
+    measure_x.append(z.copy())
+    filter_x.append(kf_hand.x_post.copy())
+    pred_x.append(kf_hand.get_prediction(u=acc_hand)[0])
+
+    print(time.time()-s_time)
+
+
+
+    # print("p", kf_hand.P)
+    # print("q", kf_hand.Q)
+    # print("R", kf_hand.R)
+    # print("K", kf_hand.K)
+    #
+    # print("-------------------------------------")
+
+
+measure_x = np.asarray(measure_x)
+filter_x = np.asarray(filter_x)
+pred_x = np.asarray(pred_x)
+
+
+for j in range(3):
+    plt.plot(timesteps[1:], measure_x[:,j], 'r--')
+    plt.plot(timesteps[1:], filter_x[:,j], 'bs')
+    plt.plot(timesteps[2:], pred_x[:-1,j], 'g^')
+
+    plt.show()
+
+
+
+
+
 
 
 
